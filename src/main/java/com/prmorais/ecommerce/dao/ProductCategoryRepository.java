71d755bc5d4e9730b4c5af019703e3cb65c65151
PgmAdmin <prmorais1302@gmail.com>
2023-09-10 21:47:56 -0300

    Initial commit

---
+++ src/main/java/com/prmorais/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.prmorais.ecommerce.dao;
+
+import com.prmorais.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
---
+++ src/main/java/com/prmorais/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.prmorais.ecommerce.dao;
+
+import com.prmorais.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
