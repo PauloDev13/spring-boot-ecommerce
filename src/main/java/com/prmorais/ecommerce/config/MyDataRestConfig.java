f436daaf107c4bcbcbc13709ca8b6dfb807db2e4
PgmAdmin <prmorais1302@gmail.com>
2023-09-13 17:59:14 -0300

    Implementing Product Category expose Ids

--- src/main/java/com/prmorais/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/prmorais/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,27 @@ package com.prmorais.ecommerce.config;
 
 import com.prmorais.ecommerce.entity.Product;
 import com.prmorais.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+  private final EntityManager entityManager;
+
+  public MyDataRestConfig(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+
   @Override
   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
     HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -24,5 +37,27 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         .forDomainType(ProductCategory.class)
         .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
         .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    // chama método auxiliar interno
+    exposeIds(config);
+  }
+
+  private void exposeIds(RepositoryRestConfiguration config) {
+    // expõe' Ids da entidade
+
+    //pega uma lista de todas as classes de entidade do gerenciador de entidade
+    Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+    // cria um array das entidades
+    List<Class> entityClasses = new ArrayList<>();
+
+    // pega as entidades do array entities
+    for (EntityType tempEntityType: entities) {
+      entityClasses.add(tempEntityType.getJavaType());
+    }
+
+    // exp~e os IDs da entidade para uma matriz de tipos de entidade/domínio
+    Class[] domainTypes = entityClasses.toArray(new Class[0]);
+    config.exposeIdsFor(domainTypes);
   }
 }
