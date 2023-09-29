77f31678e0c099bfddc2be8ecabbe7ab0d4f61d0
PgmAdmin <prmorais1302@gmail.com>
2023-09-28 21:42:18 -0300

    Implements Oauth0 with OICD

--- src/main/java/com/prmorais/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/prmorais/ecommerce/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.prmorais.ecommerce.config;
 
-import com.okta.spring.boot.oauth.Okta;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
+  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+  private String issuer;
+  @Value("${auth0.audience}")
+  private String audience;
+
   @Bean
   protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
@@ -18,10 +25,12 @@ public class SecurityConfig {
         .authenticated()
         .anyRequest().permitAll()
         )
-        .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(withDefaults()))
+        .cors(Customizer.withDefaults())
+        .oauth2ResourceServer(oauth2ResourceServer ->
+            oauth2ResourceServer.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())));
     //add CORS filters
-    .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configure(httpSecurityCorsConfigurer.disable()))
-    .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.configure(httpSecurityCsrfConfigurer.disable()));
+//    .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configure(httpSecurityCorsConfigurer.disable()))
+//    .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.configure(httpSecurityCsrfConfigurer.disable()));
 
     //add content negotiation strategy
 //    .setSharedObject(ContentNegotiationStrategy.class, new
@@ -29,10 +38,23 @@ public class SecurityConfig {
 
     //force a non-empty response body for 401's to make the response more friendly
 
-    Okta.configureResourceServer401ResponseBody(http);
+//    Okta.configureResourceServer401ResponseBody(http);
 
     // disable CSRF since we are not using Cookies for session tracking
 
     return http.build();
+  }
+
+  @Bean
+  JwtDecoder jwtDecoder() {
+    NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+
+    OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+    OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+    jwtDecoder.setJwtValidator(withAudience);
+
+    return jwtDecoder;
   }
 }
